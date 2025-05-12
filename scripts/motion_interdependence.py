fc32a86283a23bb45867c5b803683ab1512bbc34
Vincent Edwards <mister.man489@gmail.com>
2025-05-12 10:49:51 -0700

    Include 0 in subscripts for initial velocity components

--- scripts/motion_interdependence.py
+++ scripts/motion_interdependence.py
@@ -3,34 +3,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Plot horizontal position over time as vertical velocity changes
-v_x = 0.5
-v_y_values = np.linspace(0, 1.5, 7)
+v_0x = 0.5
+v_0y_values = np.linspace(0, 1.5, 7)
 
 fig, ax = plt.subplots()
-ax.set(ylabel="$x$", xlabel="$t$", title=f"$v_x$ = {v_x:.2f}")
+ax.set(ylabel="$x$", xlabel="$t$", title=f"$v_{{0x}}$ = {v_0x:.2f}")
 
-for v_y in v_y_values:
-    t, u = projectile.launch([v_x, v_y])
+for v_0y in v_0y_values:
+    t, u = projectile.launch([v_0x, v_0y])
     x = u[:, 0]
     ax.plot(t, x)
 
-fig.legend(v_y_values, title="$v_y$", loc="center right")
+fig.legend(v_0y_values, title="$v_{0y}$", loc="center right")
 fig.tight_layout()
 fig.savefig("media/x_vs_t.svg")
 
 
 # Plot vertical position over time as horizontal velocity changes
-v_y = 0.5
-v_x_values = np.linspace(0, 1.5, 7)
+v_0y = 0.5
+v_0x_values = np.linspace(0, 1.5, 7)
 
 fig, ax = plt.subplots()
-ax.set(ylabel="$y$", xlabel="$t$", title=f"$v_y$ = {v_y:.2f}")
+ax.set(ylabel="$y$", xlabel="$t$", title=f"$v_{{0y}}$ = {v_0y:.2f}")
 
-for v_x in v_x_values:
-    t, u = projectile.launch([v_x, v_y])
+for v_0x in v_0x_values:
+    t, u = projectile.launch([v_0x, v_0y])
     y = u[:, 1]
     ax.plot(t, y)
 
-fig.legend(v_x_values, title="$v_x$")
+fig.legend(v_0x_values, title="$v_{0x}$")
 fig.tight_layout()
 fig.savefig("media/y_vs_t.svg")
